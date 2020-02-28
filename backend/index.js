c0dd7979756df2dd3dbd5ad71d6b9ad9316ea736
Kamil <kamiwal92@gmail.com>
2020-02-29 00:06:56 +0100

    simple game

---
+++ backend/index.js
@@ -0,0 +1,28 @@
+let express = require("express");
+let app = express();
+let http = require("http").createServer(app);
+let io = require("socket.io")(http);
+
+app.get("/tictac", function(req, res){
+	res.sendFile("index.html", {root: "../game"});
+});
+
+app.use("/tictac", express.static("../game"));
+
+io.on("connection", function(socket){
+	console.log("connected");
+	socket.on("disconnect", function(){
+		console.log("disconnected");
+	});
+	socket.on("doThing", function(msg){
+		console.log("message: "+msg['rowNum']);
+		socket.broadcast.emit("selectedPos", msg);
+	});
+	socket.on("clear", function(msg){
+		socket.broadcast.emit("clear", "clear");
+	});
+});
+
+http.listen(3000, function(){
+	console.log("Server listen on port 3000");
+});
---
+++ game/board.js
@@ -0,0 +1,129 @@
+let board = [[0,0,0],[0,0,0],[0,0,0]];
+let actualChar = "O";
+let socket = io();
+function cleanScoreAndDrawBoard(){
+	board = [[0,0,0],[0,0,0],[0,0,0]];
+	cleanScore();
+	drawBoard();
+	actualChar = "O";
+	socket.emit("clear", "clear");
+}
+
+function drawBoard(){
+	let boardSpace = document.getElementById("board");
+	boardSpace.innerHTML = "";
+	for(r = 0; r < 3; r++){
+		let row = document.createElement('div');
+		row.id = "row_"+r;
+		row.className = "row";
+		for(c = 0; c < 3; c++){
+			let cell = document.createElement('div');
+			cell.id = r+"_"+c;
+			cell.className = "cell";
+			cell.addEventListener("click", doThing, false);
+			row.appendChild(cell);
+		}
+		boardSpace.appendChild(row);
+	}
+}
+socket.on("selectedPos", function(msg){
+	let row = msg['rowNum'];
+	let col = msg['colNum'];
+	console.log("other player selection: "+row+", "+col);
+	board[row][col] = actualChar;
+	drawSign(row+"_"+col);
+        let winner = checkBoard();
+        if(winner == "O"){
+                console.log("O is winner");
+                winnerO();
+                board = [[0,0,0],[0,0,0],[0,0,0]];
+        }
+        if(winner == "X"){
+                console.log("X is winner");
+                winnerX();
+                board = [[0,0,0],[0,0,0],[0,0,0]];
+        }
+        changeSign();
+	
+});
+socket.on("clear", function(msg){cleanScoreAndDrawBoard()});
+function doThing(e){
+	let clickedCell = e.srcElement.id;
+	let positionClicked = clickedCell.split("_");
+	let row = positionClicked[0];
+	let col = positionClicked[1];
+	console.log("Selected position: "+positionClicked);
+	socket.emit("doThing", {rowNum: row, colNum:col});
+	if(board[row][col] != 0){return;}
+	drawSign(clickedCell);
+	board[row][col] = actualChar;
+	console.log("actual board:"+ board);
+	let winner = checkBoard();
+	if(winner == "O"){
+		console.log("O is winner");
+		winnerO();
+		board = [[0,0,0],[0,0,0],[0,0,0]];
+	}
+	if(winner == "X"){
+		console.log("X is winner");
+		winnerX();
+		board = [[0,0,0],[0,0,0],[0,0,0]];
+	}
+	changeSign();
+}
+
+function drawSign(cellId){
+	let element = document.getElementById(cellId);
+	element.innerHTML = "<b id=\"char\">"+actualChar+"</b>";
+}
+
+function changeSign(){
+	if(actualChar == "O"){
+		actualChar = "X";
+	}else{
+		actualChar = "O";
+	}
+}
+
+function checkBoard(){
+	for(row = 0; row < board.length; row ++){
+		if (board[row].every((val, i , arr )=> val == "X")) return "X";
+		if (board[row].every((val, i, arr) => val == "O")) return "O";
+	}
+	
+	for(col = 0; col < board.length; col ++){//TODO zmiana 
+		let colToCheck = [];
+		for(row = 0; row < board.length; row ++){
+			colToCheck.push(board[row][col]);
+		}
+		console.log("check Column: "+colToCheck);
+		if(colToCheck.every((val ,i , arr) => val == "X")) return "X";
+		if(colToCheck.every((val, i, arr) => val == "O")) return "O";
+	}
+	let diagToCheck = [];
+	diagToCheck.push(board[0][0]);
+	diagToCheck.push(board[2][2]);
+	diagToCheck.push(board[1][1]);
+	if(diagToCheck.every((val ,i , arr) => val == "X")) return "X";
+	if(diagToCheck.every((val ,i , arr) => val == "O")) return "O";
+	diagToCheck = [];
+	diagToCheck.push(board[2][0]);
+	diagToCheck.push(board[1][1]);
+	diagToCheck.push(board[0][2]);
+	if(diagToCheck.every((val ,i , arr) => val == "X")) return "X";
+	if(diagToCheck.every((val ,i , arr) => val == "O")) return "O";
+	return 0;
+}
+
+function winnerX(){
+	let winnerLabel = document.getElementById("winner");
+	winnerLabel.innerHTML = "<b>X</b> is winner";
+}
+function winnerO(){
+	let winnerLabel = document.getElementById("winner");
+	winnerLabel.innerHTML = "<b>O</b> is winner";
+}
+function cleanScore(){
+	let winnerLabel = document.getElementById("winner");
+	winnerLabel.innerHTML="Play!";
+}
